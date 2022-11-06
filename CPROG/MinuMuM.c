63c5aa5b614283160dcc204d51aef7b006198c8d
pretty_player <85870645+preety-player@users.noreply.github.com>
2022-11-06 12:25:42 +0530

    Create MinuMuM.c

---
+++ CPROG/MinuMuM.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+long num;
+printf("Enter the number :");
+scanf("%ld",&num);
+int min=num%10;
+while(0<num)
+{
+if(min<num%10)
+{
+min=num%10;
+num=num/10;
+}
+else
+{
+num=num/10;
+}
+}
+printf("\nMin Number Is :%d \n",min);
+}
